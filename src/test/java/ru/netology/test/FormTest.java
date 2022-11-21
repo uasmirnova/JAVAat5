97993f48ceb1363dac6a26092235232d167f3d9b
uasmirnova <uasmirnova21@yandex.ru>
2022-11-21 18:20:46 +0300

    allure added

--- src/test/java/ru/netology/test/FormTest.java
+++ src/test/java/ru/netology/test/FormTest.java
@@ -1,6 +1,10 @@
 package ru.netology.test;
 
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
@@ -16,9 +20,19 @@ import static ru.netology.testinfo.DataGenerator.user;
 
 public class FormTest {
 
+    @BeforeAll
+    public static void setUpAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
     @BeforeEach
     public void setUp() {
         open("http://localhost:9999/");
+    }
+
+    @AfterAll
+    public static void tearDownAll() {
+        SelenideLogger.removeListener("allure");
     }
 
     @Test
